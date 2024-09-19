3606aa6a46cc0d789b0825d7e9990f834fb28860
binnjuu <109500598+binnjuu@users.noreply.github.com>
2024-09-19 11:17:52 +0800

    新增作業檔案並連結至html，完成作業: 數值與變數

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////
+// Part 1 作業
+// https://codingheroes.io/assignments/javascript-fundamentals-part-1
+
+// 數值與變數
+// https://codingheroes.io/assignments/values-and-variables
+
+let country = "Taiwan";
+let continent = "Taipei";
+let population = 2.495;
+
+let message = `國家: ${country}, 縣市: ${continent}, 人口數(百萬): ${population}`;
+
+console.log(message);

df756097433aa93f63216a07d5eb7b731030d92c
binnjuu <109500598+binnjuu@users.noreply.github.com>
2024-09-21 05:47:05 +0800

    作業4完成 基礎運算符

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,7 @@
 // Part 1 作業
 // https://codingheroes.io/assignments/javascript-fundamentals-part-1
 ////////////////////////////////////////////////////////
-// 數值與變數
+// 01 數值與變數
 // https://codingheroes.io/assignments/values-and-variables
 
 // let country = "Taiwan";
@@ -12,7 +12,7 @@
 // console.log(message);
 
 ////////////////////////////////////////////////////////
-// 資料類型
+// 02 資料類型
 // https://codingheroes.io/assignments/data-types
 // let isIsland = true;
 // let language = "";
@@ -21,11 +21,27 @@
 // console.log(typeof language);
 
 ////////////////////////////////////////////////////////
-// let、const、var
+// 03 let、const、var
 // https://codingheroes.io/assignments/let-const-and-var
-let num = 1; // let是可更改的變數
-num = 2;
-const PORT = 3000; // const是不可更改的變數
-PORT = 5000;
-var old = ""; // var是過時的宣告方式
-dontdothis = ""; // 建議避免這種宣告方式
+// let num = 1; // let是可更改的變數
+// num = 2;
+// const PORT = 3000; // const是不可更改的變數
+// PORT = 5000;
+// var old = ""; // var是過時的宣告方式
+// dontdothis = ""; // 建議避免這種宣告方式
+
+////////////////////////////////////////////////////////
+// 04 基礎運算符
+// https://codingheroes.io/assignments/basic-operators
+let country = "Taiwan";
+let continent = "Taipei";
+let population = 2.495;
+let language = "Mandarin";
+
+let half = population / 2;
+let add1 = population + 1;
+console.log(population > 6);
+console.log(population < 33);
+
+let description = `${continent} is in ${country}, and its ${population} million people speak ${language}`;
+console.log(description);

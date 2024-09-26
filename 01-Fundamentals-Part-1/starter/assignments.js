eebaf81e2ebbeadfeb58dbea210abda6d88bc6c3
binnjuu <109500598+binnjuu@users.noreply.github.com>
2024-09-26 10:14:32 +0800

    完成作業11 三元運算子

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -116,23 +116,30 @@ let language = "Mandarin";
 // 10 switch表達式
 // https://codingheroes.io/assignments/the-switch-statement.html
 
-switch (language.toLowerCase()) {
-  case "chinese":
-  case "mandarin":
-    console.log("MOST number of native speakers!");
-    break;
-  case "spanish":
-    console.log("2nd place in number of native speakers");
-    break;
-  case "english":
-    console.log("3rd place");
-    break;
-  case "hindi":
-    console.log("Number 4");
-    break;
-  case "arabic":
-    console.log("5th most spoken language");
-    break;
-  default:
-    console.log("Great language too :D");
-}
+// switch (language.toLowerCase()) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too :D");
+// }
+
+////////////////////////////////////////////////////////
+// 11 三元運算子
+// https://codingheroes.io/assignments/the-conditional-ternary-operator
+console.log(
+  `${country}'s population is ${population > 33 ? "above" : "below"} average`
+);
